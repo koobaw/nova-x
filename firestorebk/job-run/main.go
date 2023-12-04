9047dae3fe90e0b79a9afe37824b9a717595ba27
j.zhou <124861647+tcs2340445@users.noreply.github.com>
2023-12-04 18:00:52 +0900

    sd

--- firestorebk/job-run/main.go
+++ firestorebk/job-run/main.go
@@ -20,28 +20,30 @@ func main() {
 
 	// 创建 Firestore Admin 客户端
 	client, err := admin.NewFirestoreAdminClient(ctx)
+	log.Print("AAAAAAAAAAAAAA")
 	if err != nil {
 		log.Fatalf("Failed to create Firestore Admin client: %v", err)
 	}
+	log.Print("BBBBBBBBBBBB")
 	defer client.Close()
-
+	log.Print("CCCCCCCCCCCC")
 	// 构造备份请求
 	request := &adminpb.ExportDocumentsRequest{
 		Name:            fmt.Sprintf("projects/%s/databases/%s", projectID, databaseID),
 		CollectionIds:   nil, // 备份整个数据库
 		OutputUriPrefix: fmt.Sprintf("gs://%s/firestore-backup", bucketName),
 	}
-
+	log.Print("AAAAAAAAAAAAAA")
 	// 执行备份
 	op, err := client.ExportDocuments(ctx, request)
 	if err != nil {
 		log.Fatalf("Failed to export documents: %v", err)
 	}
-
+	log.Print("DDDDDDDDDDDDD")
 	// 等待备份完成
 	if _, err := op.Wait(ctx); err != nil {
 		log.Fatalf("Failed to wait for export operation to complete: %v", err)
 	}
-
+	log.Print("EEEEEEEEEEEEE")
 	log.Print("Firestore database backup completed successfully!")
 }
