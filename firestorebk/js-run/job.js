b1683e5e0afc907d434f04c3cbdc01c888c8dbc0
j.zhou <124861647+tcs2340445@users.noreply.github.com>
2023-12-05 15:18:04 +0900

    sd

--- firestorebk/js-run/job.js
+++ firestorebk/js-run/job.js
@@ -1,15 +1,19 @@
 const firestore = require('@google-cloud/firestore');
-const { Storage } = require("@google-cloud/storage");
+
+const BUCKET_NAME = 'nova-hj-job';
+const PROJECT_ID = 'nova-hj';
+const DATABASE_INSTANCE = '(default)';
 
 async function main() {
   try {
-    console.log("CCCCCCCCCCCCCC");
+    console.log("FirestoreAdminClient 作成");
 
     const client = new firestore.v1.FirestoreAdminClient();
-    const bucket = 'gs://nova-hj-job';
-    const databaseName = client.databasePath('nova-hj', '(default)');
+    
+    const bucket = `gs://${BUCKET_NAME}`;
+    const databaseName = client.databasePath(PROJECT_ID, DATABASE_INSTANCE);
 
-    console.log("AAAAAAAAAAAAAAA");
+    console.log("exportDocuments 実行。。。");
 
     const responses = await client.exportDocuments({
       name: databaseName,
@@ -19,8 +23,8 @@ async function main() {
 
     const response = responses[0];
 
-    console.log("BBBBBBBBBBBBBB");
-    console.log(`Operation Name KKKKKKK: ${response['name']}`);
+    console.log("exportDocuments 完了");
+    console.log(`操作Name: ${response['name']}`);
   } catch (err) {
     console.error(err);
     process.exit(1); // Exit with an error code
