1b6a7faee0e5bbd4cd6d81f36dc485c59138a365
j.zhou <124861647+tcs2340445@users.noreply.github.com>
2023-12-05 15:48:59 +0900

    goback

--- firestorebk/js-run/job.js
+++ firestorebk/js-run/job.js
@@ -1,64 +1,33 @@
 const firestore = require('@google-cloud/firestore');
 
-// 定数を定義
-const BUCKET_NAME = 'nova-hj-job'; // バケット名
-const PROJECT_ID = 'nova-hj';      // プロジェクトID
-const DATABASE_INSTANCE = '(default)'; // データベースインスタンス
+const BUCKET_NAME = 'nova-hj-job';
+const PROJECT_ID = 'nova-hj';
+const DATABASE_INSTANCE = '(default)';
 
-// Firestoreクライアントの作成
-function createFirestoreClient() {
-  return new firestore.v1.FirestoreAdminClient();
-}
-
-// バケット名の作成
-function createBucketName() {
-  return `gs://${BUCKET_NAME}`;
-}
-
-// データベース名の作成
-function createDatabaseName(client) {
-  return client.databasePath(PROJECT_ID, DATABASE_INSTANCE);
-}
-
-// ドキュメントのエクスポート
-async function exportFirestoreDocuments(client, databaseName, bucket) {
-  console.log("exportDocuments 実行。。。");
+async function main() {
   try {
+    console.log("FirestoreAdminClient 作成");
+
+    const client = new firestore.v1.FirestoreAdminClient();
+    
+    const bucket = `gs://${BUCKET_NAME}`;
+    const databaseName = client.databasePath(PROJECT_ID, DATABASE_INSTANCE);
+
+    console.log("exportDocuments 実行。。。");
+
     const responses = await client.exportDocuments({
       name: databaseName,
       outputUriPrefix: bucket,
       collectionIds: []
     });
 
-    if (!responses || !responses.length || !responses[0] || !responses[0].name) {
-      throw new Error("エクスポートの応答が無効です。");
-    }
-
     const response = responses[0];
+
     console.log("exportDocuments 完了");
-    console.log(`操作Name operations ID : ${response.name}`);
+    console.log(`操作Name operations ID : ${response['name']}`);
   } catch (err) {
-    console.error("エクスポート中にエラーが発生しました:", err.message);
-    throw err; // エラーを再スロー
-  }
-}
-
-async function main() {
-  try {
-    console.log("FirestoreAdminClient 作成");
-
-    // Firestoreクライアントの作成
-    const client = createFirestolient();
-    
-    // バケットとデータベース名の作成
-    const bucket = createBucketName();
-    const databaseName = createDatabaseName(client);
-
-    // ドキュメントのエクスポート
-    await exportFirestoreDocuments(client, databaseName, bucket);
-  } catch (err) {
-    console.error("メイン関数でエラーが発生しました:", err.message);
-    process.exit(1); // エラーコードでプロセスを終了
+    console.error(err);
+    process.exit(1); // Exit with an error code
   }
 }
 
